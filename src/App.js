20eb40ac26d628fc36ac7bce689f32a6f0dd376b
mmataja <mmataja92@gmail.com>
2019-10-06 20:56:58 +0200

    add material-ui package

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <React.Fragment>
+
+    </React.Fragment>
   );
 }
 
