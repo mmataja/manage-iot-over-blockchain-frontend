7305b26bbf4266346bc267ed5dae5f6e3583ca77
mmataja <mmataja92@gmail.com>
2019-12-30 18:58:16 +0100

    Rewrite to function component

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core/styles'
-import { Container, TextField, Button } from '@material-ui/core';
+import { TextField, Button, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { Styles } from './styles';
 
-const theme = createMuiTheme();
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+    marginLeft: 240,
+    alignItems: 'center',
+  }, 
+}));
 
-export class RegisterForm extends Component {
-  state = {
+const RegisterForm = () => {
+  const classes = useStyles();
+
+  const [formData, setFormData] = useState({
     deviceOwner: '',
     deviceName: '',
     deviceFirmware: '',
     url: '',
     publicKey: '',
-  };
+  });
 
-  handleChange = input => e => {
-    this.setState({ [input]: e.target.value });
+  const handleChange = input => e => {
+    setFormData({...formData, [input]: e.target.value});
   }
 
-  submitRegister = async e => {
+  const submitRegister = async e => {
     e.preventDefault();
 
-    const { deviceOwner, deviceName, deviceFirmware, publicKey, url } = this.state;
     const selectedWalletAddress = await window.web3.eth.getAccounts();
     const account = selectedWalletAddress[0];
     
     const deviceData = JSON.stringify({
-      deviceOwner,
-      deviceName,
-      deviceFirmware,
+      deviceOwner: formData.deviceOwner,
+      deviceName: formData.deviceName,
+      deviceFirmware: formData.deviceFirmware,
       account,
     });
 
@@ -40,93 +49,78 @@ export class RegisterForm extends Component {
     await axios.post("http://localhost:8000/register", {
       account,
       signature, 
-      url,
-      publicKey,
-      deviceOwner,
-      deviceName,
+      url: formData.url,
+      publicKey: formData.publicKey,
+      deviceOwner: formData.deviceOwner,
+      deviceName: formData.deviceName,
     }).then(response => console.log(response))
       .catch(error => console.log(error));
   }
 
-  render() {
-    const { deviceOwner, deviceName, deviceFirmware, url, publicKey } = this.state;
-
-    return (
-      <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <Container maxWidth="sm">
-            <form onSubmit={this.submitRegister}>
-              <TextField 
-                id="name"
-                label="Name"
-                margin="normal"
-                required={true}
-                onChange={this.handleChange('deviceName')}
-                value={deviceName}
-                style={Styles.textField}
-              />
-              <br/>
-              <TextField 
-                id="owner"
-                label="Owner"
-                margin="normal"
-                onChange={this.handleChange('deviceOwner')}
-                value={deviceOwner}
-                style={Styles.textField}
-              />
-              <br/>
-              <TextField 
-                id="firmware"
-                label="Firmware Version"
-                margin="normal"
-                onChange={this.handleChange('deviceFirmware')}
-                value={deviceFirmware}
-                style={Styles.textField}
-              />
-              <br/>
-              <TextField 
-                id="url"
-                label="URL"
-                margin="normal"
-                onChange={this.handleChange('url')}
-                value={url}
-                style={Styles.textField}
-              />
-              <br/>
-              <TextField 
-                id="public-key"
-                label="Public Key"
-                multiline
-                rows="6"
-                margin="normal"
-                onChange={this.handleChange('publicKey')}
-                value={publicKey}
-                style={Styles.textField}
-                variant="outlined"
-              />
-              <br/>
-              <Button
-                variant="contained"
-                type="submit" 
-                size="medium"
-                color="primary"
-                style={styles.button, Styles.textField}
-              >
-                Register Device
-              </Button>
-            </form>
-
-          </Container>
-        </React.Fragment>
-      </ThemeProvider>
-    )
-  }
+  return (
+    <Paper className={classes.paper}>
+      <form onSubmit={submitRegister}>
+        <TextField 
+          id="name"
+          label="Name"
+          margin="normal"
+          required={true}
+          onChange={handleChange('deviceName')}
+          value={formData.deviceName}
+          style={Styles.textField}
+        />
+        <br/>
+        <TextField 
+          id="owner"
+          label="Owner"
+          margin="normal"
+          onChange={handleChange('deviceOwner')}
+          value={formData.deviceOwner}
+          style={Styles.textField}
+        />
+        <br/>
+        <TextField 
+          id="firmware"
+          label="Firmware Version"
+          margin="normal"
+          onChange={handleChange('deviceFirmware')}
+          value={formData.deviceFirmware}
+          style={Styles.textField}
+        />
+        <br/>
+        <TextField 
+          id="url"
+          label="URL"
+          margin="normal"
+          onChange={handleChange('url')}
+          value={formData.url}
+          style={Styles.textField}
+        />
+        <br/>
+        <TextField 
+          id="public-key"
+          label="Public Key"
+          multiline
+          rows="6"
+          margin="normal"
+          onChange={handleChange('publicKey')}
+          value={formData.publicKey}
+          style={Styles.textField}
+          variant="outlined"
+        />
+        <br/>
+        <Button
+          variant="contained"
+          type="submit" 
+          size="medium"
+          color="primary"
+          style={Styles.textField}
+        >
+          Register Device
+        </Button>
+      </form>
+    </Paper>
+  ) 
 }
 
-const styles = {
-  button: {
-    margin: 15
-  }
-}
-
-export default RegisterForm; 
+export default RegisterForm;
